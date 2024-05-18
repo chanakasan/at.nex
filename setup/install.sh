db33c8442d4d23dfdffec501fd3e665edf9a7b89
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-18 15:52:00 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -3,5 +3,6 @@ set -e
 tmp_root=$HOME/dotfiles/at.nex
 tmp_support=$tmp_root/.impl/support
 source $tmp_support/install/helpers.sh
+source $tmp_support/install/run_default.sh
 
 run_default "at.nex"
