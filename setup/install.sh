0d07b0f1c08ca2b53be5cc5fb013d03b39bf2799
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-18 17:31:03 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -2,4 +2,4 @@ set -e
 
 export tmp_root=$HOME/dotfiles/at.nex
 export tmp_support=$tmp_root/.impl/support
-bash $tmp_support/install/run_default.sh "at.nex"
+bash $tmp_support/installer/run_default.sh "at.nex"
