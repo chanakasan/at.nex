8487e32f76a84e7658e22071d83e663cb898b07f
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-18 14:16:40 +0530

    rev

--- src/init/at.sh
+++ src/init/at.sh
@@ -1,9 +1,46 @@
 @ () {
   local base=$nx_at_root
   local input="$1"
-  if [ -z "$input" ]; then
-    echo "Hello @ !"
+  if args_empty $@; then
+    print_usage
+    return
+  elif is_match "rel" $@; then
+    at_rel
+  elif is_match "ed" $@; then
+    at_ed
   else
-    bash $base/src/cmd/run.sh "$@"
+    at_run $@
   fi
+}
+
+print_usage() {
+  echo " Hello @ !"
+}
+
+args_empty() {
+  if [ -z "$1" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+is_match() {
+  if [ "$1" == "$2" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+at_rel() {
+  source $HOME/.bashrc
+}
+
+at_ed() {
+  vim $HOME/.bashrc
+}
+
+at_run() {
+  bash $base/src/cmd/run.sh "$@"
 }
