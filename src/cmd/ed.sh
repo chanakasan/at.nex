ea1fcdfef7484059d4623a00734c5f9e1ccba179
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-19 20:11:41 +0530

    rev

--- src/cmd/ed.sh
+++ src/cmd/ed.sh
@@ -1,10 +1,13 @@
 main() {
-  if [ "$1" == "brc" ]; then
+  local main_cmd="@ ed $@"
+  if [ -z "$1" ]; then
+    vim $HOME/bashrc2
+  elif [ "$1" == "brc" ]; then
     vim $HOME/.bashrc
   elif [ "$1" == "vrc" ]; then
     vim $HOME/.vimrc
   else
-    vim $HOME/bashrc2
+    echo " not found: $main_cmd"
   fi
 }
 
