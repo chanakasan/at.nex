9dbf3ca1a46b42230ab27dc307949eda517cee59
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-19 19:15:05 +0530

    rev

--- src/entry.sh
+++ src/entry.sh
@@ -4,3 +4,11 @@ source $nx_at_root/src/init/at.sh
 if [ -f ~/bashrc2 ]; then
   source ~/bashrc2
 fi
+
+if [ -d ~/user/bash ]; then
+  for f in ~/user/bash/*.sh; do
+    if [ -f "$f" ]; then
+      source $f
+    fi
+  done
+fi
