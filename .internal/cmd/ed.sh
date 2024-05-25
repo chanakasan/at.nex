fcfc7788d6d487b16642c6e79dbc9734b730db88
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-05-25 10:03:48 +0530

    fix config

--- .internal/cmd/ed.sh
+++ .internal/cmd/ed.sh
@@ -1,7 +1,8 @@
-source $nx_at_root/src/config/config.sh
 source $nx_at_root/src/config/ed.sh
 
 main() {
+  local config_file=$HOME/user/bash/config/at.sh
+  load_config
   local editor=${nx_at_editor:-vim}
   if [ -z "$1" ]; then
     ed_default
@@ -12,12 +13,18 @@ main() {
   fi
 }
 
+load_config() {
+  if [ -f "$config_file" ]; then
+    source $config_file
+  fi
+}
+
 ed_default() {
   $editor $HOME/bashrc2
 }
 
 ed_config() {
-  $editor $nx_at_root/src/config/config.sh
+  $editor $config_file
 }
 
 ed_alias() {
