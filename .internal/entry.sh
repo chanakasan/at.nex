ca087784ea211ce8821c80f36801ea634af8a600
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-09-21 07:18:33 +0530

    rev

--- .internal/entry.sh
+++ .internal/entry.sh
@@ -4,6 +4,9 @@ source $nx_at_root/.internal/core/fsub.sh
 source $nx_at_root/.internal/core/a.sh
 source $nx_at_root/.internal/core/c.sh
 
+# wip
+source $nx_at_root/.internal/wip/one.sh
+
 # ext
 # source $nx_at_root/.internal/ext/prompt.sh
 source $nx_at_root/.internal/ext/prompt-color.sh
